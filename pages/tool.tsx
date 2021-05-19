c96661951949afe13235fa25411ffe60fa312626
김창회 <dudnqnfqlc@naver.com>
2021-05-19 20:09:38 +0900

    액자 클릭하면 마우스 커서 따라다니게 하기, + 커서 위치 항상 찾아오는 훅 추가요

--- pages/tool.tsx
+++ pages/tool.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+import { useGetCursorPosition } from '../hooks';
 
 const ToolContainer = styled.div`
   width: 100%;
   margin: 0 auto;
   display: flex;
+`;
+
+const YouSelectedFrame = styled.div<{ width: string; height: string; left: string; top: string }>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  top: ${({ top }) => top};
+  left: ${({ left }) => left};
+  border: 1px solid #333;
+  position: absolute;
+  cursor: pointer;
+  z-index: 2;
 `;
 
 const ImageWrapper = styled.div`
@@ -17,6 +29,8 @@ const ImageWrapper = styled.div`
 const VersatileWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  z-index: 3;
+  background: #fff;
 `;
 
 const Versatile = styled.div`
@@ -103,8 +117,16 @@ interface PaperSize {
   };
 }
 
+interface FramePosition {
+  left: string;
+  top: string;
+}
+
 const Tool = () => {
-  const paperSize: PaperSize[] = [
+  const imgUrl =
+    'https://lh3.googleusercontent.com/proxy/3vOprbwYSsnm5QdLaSoxy3OcTqMWYiCJo3-PKXm86gu1XmMyMiGIhgJQCnOwTsKK5K8-7uADt6lXbccdRbRyUiqT53jxj67YHmsyCwwegCyyYCnaoYs';
+
+  const [paperSize] = useState<PaperSize[]>([
     {
       name: 'a4',
       size: {
@@ -126,14 +148,41 @@ const Tool = () => {
         height: '105px',
       },
     },
-  ];
+  ]);
 
-  const imgUrl =
-    'https://lh3.googleusercontent.com/proxy/3vOprbwYSsnm5QdLaSoxy3OcTqMWYiCJo3-PKXm86gu1XmMyMiGIhgJQCnOwTsKK5K8-7uADt6lXbccdRbRyUiqT53jxj67YHmsyCwwegCyyYCnaoYs';
+  const [selectedFrame, setSelectedFrame] = useState(false);
+  const [selectedFrameInfo, setSelectedFrameInfo] = useState<PaperSize | null>(null);
+  const [cursorX, cursorY] = useGetCursorPosition(selectedFrame);
+  const [selectedFramePosition, setSelectedFramePosition] = useState<FramePosition | null>(null);
+
+  //   액자를 클릭하묜?
+  const handleFrameSelect = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      const { value } = e.currentTarget.dataset;
+      const selectedFrameName = paperSize.filter((lst) => lst.name === value);
+      setSelectedFrame(() => true);
+      setSelectedFrameInfo(selectedFrameName[0]);
+    },
+    [paperSize],
+  );
+
+  useEffect(() => {
+    if (selectedFrame && selectedFrameInfo) {
+      const {
+        size: { width, height },
+      } = selectedFrameInfo;
+      const x = cursorX - +width.replace('px', '') / 2;
+      const y = cursorY - +height.replace('px', '') / 2;
+      setSelectedFramePosition({ left: `${x}px`, top: `${y}px` });
+    }
+  }, [selectedFrame, selectedFrameInfo, cursorX, cursorY]);
 
   return (
     <ToolContainer>
-      <ImageWrapper>
+      {selectedFrame && selectedFrameInfo && selectedFramePosition && (
+        <YouSelectedFrame {...selectedFrameInfo.size} {...selectedFramePosition}></YouSelectedFrame>
+      )}
+      <ImageWrapper id="img-box">
         <div>
           <img src={imgUrl} alt="샘플이미지" />
         </div>
@@ -152,8 +201,8 @@ const Tool = () => {
 
             <FactoryTitle>액자크기</FactoryTitle>
             <FrameWrapper>
-              {paperSize.map((paper) => (
-                <FrameSize {...paper.size}>
+              {paperSize.map((paper, index) => (
+                <FrameSize key={index} data-value={paper.name} {...paper.size} onClick={handleFrameSelect}>
                   <FrameSizeName>{paper.name}</FrameSizeName>
                 </FrameSize>
               ))}
