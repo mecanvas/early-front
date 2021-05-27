a35fe04b8cc04063fa174073fc6e644a9644420c
김창회 <dudnqnfqlc@naver.com>
2021-05-27 23:14:24 +0900

    원하는 색상 없으면 컬러피커로 할 수 있게 설정

--- src/components/tool/ToolColorPalette.tsx
+++ src/components/tool/ToolColorPalette.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { ColorPaletteWrapper } from './ToolStyle';
-import { CirclePicker, ColorResult } from 'react-color';
+import { CirclePicker, ColorResult, ChromePicker } from 'react-color';
 
 interface Props {
   onChange: (color: ColorResult) => void;
@@ -8,14 +8,37 @@ interface Props {
 }
 
 const ToolColorPalette = ({ onChange, type }: Props) => {
+  const [isFreeColorPalette, setIsFreeColorPalette] = useState(false);
+  const [yourPickHex, setYourPickHex] = useState('#ffffff');
+
   const colors = {
     bg: ['#ffffff', '#333', '#D9E3F0', '#F47373', '#697689'],
     frame: ['#333', '#dbdbdb', '#F47373'],
   };
+
+  const handleFreeColor = useCallback(() => {
+    setIsFreeColorPalette((prev) => !prev);
+  }, []);
+
+  const handlePickerChange = useCallback((color: ColorResult) => {
+    setYourPickHex(color.hex);
+  }, []);
+
   return (
-    <ColorPaletteWrapper>
-      <CirclePicker onChange={onChange} colors={colors[type]} circleSpacing={7} />
-    </ColorPaletteWrapper>
+    <>
+      <ColorPaletteWrapper>
+        {isFreeColorPalette ? (
+          <ChromePicker
+            color={yourPickHex}
+            onChange={handlePickerChange}
+            onChangeComplete={onChange}
+            disableAlpha={true}
+          />
+        ) : null}
+        <CirclePicker onChange={onChange} colors={colors[type]} circleSpacing={7} />
+      </ColorPaletteWrapper>
+      <div onClick={handleFreeColor}>난 다른거..</div>
+    </>
   );
 };
 
