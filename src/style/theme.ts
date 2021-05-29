030645dab9182bf09e47eb5142b3e21d4fddb38e
김창회 <dudnqnfqlc@naver.com>
2021-05-29 22:28:15 +0900

    이모션 테마 설정

---
+++ src/style/theme.ts
@@ -0,0 +1,26 @@
+export const theme = {
+  color: {
+    white: '#fff',
+    gray000: '#f8f9fa',
+    gray100: '#f1f4f5',
+    gray200: '#e9edef',
+    gray300: '#dee4e6',
+    gray400: '#ced7da',
+    gray500: '#adb8bd',
+    gray600: '#869196',
+    gray700: '#495357',
+    gray800: '#343d40',
+    gray900: '#212729',
+    black: '#000',
+
+    primary: '#ee6f57',
+    secondary: '#1f3c88',
+    secondarydark: '#070d59',
+    secondarybg: '#f6f5f5',
+
+    blue: '#499edc',
+    red: '#d2423c',
+    yellow: '#f0af41',
+    cyan: '#47b3c9',
+  },
+};
