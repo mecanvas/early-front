79ac3811249ef54f6e08ca709289355597a90efc
김창회 <dudnqnfqlc@naver.com>
2021-06-09 22:11:48 +0900

    네트리파이 배포 오류 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,4 @@
-const { NODE_ENV, API_PORT } = process.env;
-
 export const MAX_HEIGHT = 1000;
 
-export const API_URL = NODE_ENV === 'production' ? 'https://mecanvas.herokuapp.com' : `http://localhost:${API_PORT}`;
+export const API_URL =
+  process.env.NODE_ENV === 'production' ? 'https://mecanvas.herokuapp.com' : `http://localhost:${process.env.API_PORT}`;
