980db88f1777b11655eedcd2d100cde0f35b2059
김창회 <dudnqnfqlc@naver.com>
2021-06-10 18:40:02 +0900

    개발 모드일때는 파일 사이즈 제한 X

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const MAX_HEIGHT = 1000;
 
 //  1_000_000  = 1MB
-export const IMG_LIMIT_MINIMUM_SIZE = 500_000;
+export const IMG_LIMIT_MINIMUM_SIZE = process.env.NODE_ENV === 'production' ? 500_000 : 0;
 export const API_URL =
   process.env.NODE_ENV === 'production'
     ? 'https://mecanvas.herokuapp.com'
