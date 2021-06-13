5086b88ac6ef49c69070e546fc5de3d15d34890b
김창회 <dudnqnfqlc@naver.com>
2021-06-13 18:42:12 +0900

    사진 다운로드 구현 완료

--- src/components/admin/canvasorder/AdminCanvasOrderDetail.tsx
+++ src/components/admin/canvasorder/AdminCanvasOrderDetail.tsx
@@ -1,12 +1,16 @@
 import { useRouter } from 'next/router';
-import React from 'react';
-import { Descriptions, Tag } from 'antd';
+import React, { useCallback, useState } from 'react';
+import { Descriptions, Tag, Button } from 'antd';
 import useSWR from 'swr';
 import { adminGetFetcher } from 'src/fetcher';
 import { CanvasOrderDetail } from 'src/interfaces/admin/CanvasOrderInterface';
 import Loading from 'src/components/common/Loading';
 import Img from 'src/components/common/Img';
 import { dateFormat } from 'src/utils/dateFormat';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { theme } from 'src/style/theme';
+import { ImgToDataURL } from 'src/utils/ImgToDataURL';
 
 const AdminOrderDetail = () => {
   const router = useRouter();
@@ -15,6 +19,49 @@ const AdminOrderDetail = () => {
   } = router;
 
   const { data } = useSWR<CanvasOrderDetail>(canvasOrderId ? `/canvasorder/${canvasOrderId}` : null, adminGetFetcher);
+
+  const [loading, setLoading] = useState(false);
+  const handleImgDownload = useCallback(
+    async (e) => {
+      const { cmd } = e.currentTarget.dataset;
+      if (!cmd || !data) return;
+      setLoading(true);
+      const a = document.createElement('a');
+      const { originImgUrl, canvasFrameUrls, createdAt, username, id, paperNames } = data;
+
+      const downloadOriginImg = async () => {
+        a.download = `${id}_${dateFormat(createdAt)}_${username}_원본이미지`;
+        a.href = await ImgToDataURL(`${originImgUrl}?${new Date().getTime()}`);
+        a.click();
+      };
+
+      const downloadFrameImg = async () => {
+        canvasFrameUrls.forEach(async (url: string, index: number) => {
+          const a = document.createElement('a');
+          const paperName = paperNames[index];
+          a.download = await `${id}_${dateFormat(createdAt)}_${username}_${paperName}`;
+          a.href = await ImgToDataURL(`${url}?${new Date().getTime()}`);
+          a.click();
+        });
+      };
+
+      if (cmd === 'all') {
+        downloadOriginImg();
+        downloadFrameImg();
+      }
+
+      if (cmd === 'origin') {
+        downloadOriginImg();
+      }
+
+      if (cmd === 'frame') {
+        downloadFrameImg();
+      }
+
+      setLoading(false);
+    },
+    [data],
+  );
 
   if (!data) {
     return <Loading loading={!data} />;
@@ -25,6 +72,40 @@ const AdminOrderDetail = () => {
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <h2>{data.username}님의 주문</h2>
         <div>{dateFormat(data.createdAt)}</div>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <Button
+          type="default"
+          style={{ marginRight: '6px' }}
+          loading={loading}
+          onClick={handleImgDownload}
+          data-cmd="all"
+        >
+          <FontAwesomeIcon icon={faDownload} fill={theme.color.primary} />
+          <span style={{ marginLeft: '6px' }}>모두 다운로드</span>
+        </Button>
+
+        <Button
+          type="default"
+          style={{ marginRight: '6px' }}
+          loading={loading}
+          onClick={handleImgDownload}
+          data-cmd="origin"
+        >
+          <FontAwesomeIcon icon={faDownload} fill={theme.color.primary} />
+          <span style={{ marginLeft: '6px' }}>원본 다운로드</span>
+        </Button>
+
+        <Button
+          type="default"
+          style={{ marginRight: '6px' }}
+          loading={loading}
+          onClick={handleImgDownload}
+          data-cmd="frame"
+        >
+          <FontAwesomeIcon icon={faDownload} fill={theme.color.primary} />
+          <span style={{ marginLeft: '6px' }}>액자 다운로드</span>
+        </Button>
       </div>
 
       <Descriptions
