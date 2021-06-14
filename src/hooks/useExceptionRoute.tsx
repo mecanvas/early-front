178bc5534a89cbb30a8f500f4e74b4f485304220
김창회 <dudnqnfqlc@naver.com>
2021-06-15 02:24:43 +0900

    훅 무한 렌더링 수정

--- src/hooks/useExceptionRoute.tsx
+++ src/hooks/useExceptionRoute.tsx
@@ -1,17 +1,23 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { exceptionRoutes } from 'src/constants';
 
 export const useExceptionRoute = () => {
   const { asPath } = useRouter();
   const [exceptionRoute, setExceptionRoute] = useState(false);
 
-  exceptionRoutes.forEach((route) => {
-    if (asPath === route) {
-      setExceptionRoute(true);
-      return;
-    }
-  });
+  const except = useCallback(() => {
+    exceptionRoutes.forEach((route) => {
+      if (asPath === route) {
+        setExceptionRoute(true);
+        return;
+      }
+    });
+  }, [asPath]);
+
+  useEffect(() => {
+    except();
+  }, [except]);
 
   return { exceptionRoute };
 };
