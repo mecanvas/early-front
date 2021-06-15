659bbb7a7bf94e78c9ed03388fb31c00448598cf
김창회 <dudnqnfqlc@naver.com>
2021-06-15 22:42:19 +0900

    모바일에서 액자 크기 변경

--- src/utils/cmToPx.ts
+++ src/utils/cmToPx.ts
@@ -1,4 +1,9 @@
+import { isMobile } from 'react-device-detect';
+
 export const cmToPx = (cm: number) => {
   const px = 37.7952755906;
+  if (isMobile) {
+    return (cm * px) / 4;
+  }
   return (cm * px) / 3;
 };
