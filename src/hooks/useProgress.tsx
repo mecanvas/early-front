8e5f3332864c5bf6d571eccc9ccc8fbfd7036f52
김창회 <dudnqnfqlc@naver.com>
2021-07-16 00:45:09 +0900

    이미지 로딩시 투명도 추가 및 퍼센테지 적용안되는거 수정

--- src/hooks/useProgress.tsx
+++ src/hooks/useProgress.tsx
@@ -1,16 +1,20 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
+import { useGlobalState } from '.';
 
 interface ProgressProps {
   progress?: number;
 }
 
 export const useProgress = () => {
-  const [progressPercentage, setProgressPercentage] = useState(0);
+  const [progressPercentage, setProgressPercentage] = useGlobalState<number>('progressPercentage', 0);
 
-  const getProgressGage = useCallback((data: ProgressEvent) => {
-    const percentage = Math.round((100 * data.loaded) / data.total);
-    setProgressPercentage(percentage);
-  }, []);
+  const getProgressGage = useCallback(
+    (data: ProgressEvent) => {
+      const percentage = Math.round((100 * data.loaded) / data.total);
+      setProgressPercentage(percentage);
+    },
+    [setProgressPercentage],
+  );
 
   const ProgressBar = ({ progress }: ProgressProps) => {
     return <div>{!progress ? progressPercentage : progress}</div>;
