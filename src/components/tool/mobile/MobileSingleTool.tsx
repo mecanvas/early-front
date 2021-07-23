8278e2ffcbda10e627588e1adc9ec1b80c3cff96
김창회 <dudnqnfqlc@naver.com>
2021-07-24 07:50:23 +0900

    제작과 시안 구현

--- src/components/tool/mobile/MobileSingleTool.tsx
+++ src/components/tool/mobile/MobileSingleTool.tsx
@@ -1,6 +1,8 @@
+import { ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { Button, List, Steps, Tabs } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, List, Slider, Steps, Tabs } from 'antd';
 import { icons } from 'public/icons';
 import React, { useCallback, useMemo, useState } from 'react';
 import ImageDropZone from 'src/components/common/ImageDropZone';
@@ -168,12 +170,10 @@ const TabTitle = styled.span`
 `;
 
 const SecondsContent = styled.div`
-  @media all and (min-width: ${({ theme }) => theme.size.sm}) {
-    display: grid;
-    gap: 1em;
-    grid-template-columns: repeat(2, 1fr);
-    justify-content: center;
-  }
+  display: grid;
+  gap: 1em;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
   @media all and (max-width: ${({ theme }) => theme.size.sm}) {
     width: 100%;
     display: grid;
@@ -226,6 +226,110 @@ const SecondsFramePreview = styled.div<{ width: string; height: string }>`
   }
 `;
 
+const ThirdContent = styled.div`
+  display: grid;
+  gap: 1em;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
+  @media all and (max-width: ${({ theme }) => theme.size.sm}) {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    justify-content: space-between;
+  }
+`;
+
+const ThirdContentDrawingCanvas = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.color.gray500};
+  /* background-color: ${({ theme }) => theme.color.white}; */
+`;
+
+const ThirdContentCropper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 285px;
+  height: 285px;
+  transform: translate(-50%, -50%);
+  cursor: move;
+
+  /* top */
+  span:nth-of-type(1) {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 1px;
+    border-top: 2px dashed ${({ theme }) => theme.color.primary};
+  }
+  /* right */
+  span:nth-of-type(2) {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 1px;
+    height: 100%;
+    border-right: 2px dashed ${({ theme }) => theme.color.primary};
+  }
+  /* bottom */
+  span:nth-of-type(3) {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    border-bottom: 2px dashed ${({ theme }) => theme.color.primary};
+  }
+  /* left */
+  span:nth-of-type(4) {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 1px;
+    height: 100%;
+    border-left: 2px dashed ${({ theme }) => theme.color.primary};
+  }
+`;
+
+// TODO: 캔버스로 전환 예정 이미지 자를 수 있게.
+const LikeCanvas = styled.div`
+  width: 100%;
+  max-width: 462.5px;
+  background-color: #a5b0e6;
+  filter: brightness(70%);
+
+  img {
+    width: 100%;
+  }
+`;
+
+const ThirdPreviewCanvasWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 6em;
+  background-color: ${({ theme }) => theme.color.gray000};
+
+  @media all and (max-width: ${({ theme }) => theme.size.sm}) {
+    padding: 6em;
+  }
+`;
+
+const ThirdPreviewCanvas = styled.div`
+  width: 100%;
+  max-width: 302px;
+  filter: ${({ theme }) => theme.canvasShadowFilter};
+  background-color: #a5b0e6;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 const MobileSingleTool = () => {
   const FIRST_INDEX = 0;
   const LAST_INDEX = 3;
@@ -237,10 +341,10 @@ const MobileSingleTool = () => {
   }, []);
   const stepTitle = useMemo(() => {
     const title = new Map();
-    title.set(0, '이미지 선택');
-    title.set(1, '액자 선택');
-    title.set(2, '시안 확인');
-    title.set(3, '저장하기');
+    title.set(0, '이미지');
+    title.set(1, '액자');
+    title.set(2, '제작');
+    title.set(3, '저장');
     return title;
   }, []);
 
@@ -384,7 +488,43 @@ const MobileSingleTool = () => {
           </MobileContent>
         )}
         {/* step3 */}
-        {stepCount === 2 && <MobileContent>시안 확인</MobileContent>}
+        {stepCount === 2 && (
+          <MobileContent>
+            <div>
+              <Slider
+                style={{ width: '135px' }}
+                min={1}
+                defaultValue={1}
+                max={10}
+                step={1}
+                marks={{ 1: <ZoomOutOutlined />, 10: <ZoomInOutlined /> }}
+              />
+            </div>
+            <ThirdContent>
+              <ThirdContentDrawingCanvas>
+                <LikeCanvas>
+                  <img src="https://early-dev.s3.ap-northeast-2.amazonaws.com/brian-lawson-RBy6FEQ2DIk-unsplash-removebg-preview.png" />
+                </LikeCanvas>
+                <ThirdContentCropper>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </ThirdContentCropper>
+              </ThirdContentDrawingCanvas>
+
+              <ThirdPreviewCanvasWrapper>
+                <ThirdPreviewCanvas>
+                  <img
+                    src={
+                      'https://early-dev.s3.ap-northeast-2.amazonaws.com/20210718174824_2021%E1%84%82%E1%85%A7%E1%86%AB+07%E1%84%8B%E1%85%AF%E1%86%AF+18%E1%84%8B%E1%85%B5%E1%86%AF+17_48_gg_%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%87%E1%85%A1%E1%86%BC+S-1%E1%84%92%E1%85%A9.png'
+                    }
+                  />
+                </ThirdPreviewCanvas>
+              </ThirdPreviewCanvasWrapper>
+            </ThirdContent>
+          </MobileContent>
+        )}
         {/* step4 */}
         {stepCount === 3 && <MobileContent>저장하기</MobileContent>}
 
