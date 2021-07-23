49cf1e0071156fd8c78625fd0cb3544932819ae5
김창회 <dudnqnfqlc@naver.com>
2021-07-24 03:23:23 +0900

    모바일 전용 컴포넌트 생성

---
+++ src/components/tool/mobile/MobileSingleTool.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MobileSingleTool = () => {
+  return <div></div>;
+};
+
+export default MobileSingleTool;
--- src/pages/tool/single.tsx
+++ src/pages/tool/single.tsx
@@ -1,7 +1,15 @@
 import dynamic from 'next/dynamic';
+import { isMobile } from 'react-device-detect';
 import Loading from 'src/components/common/Loading';
 const SingleTool = dynamic(() => import('src/components/tool/single/SingleTool'), {
   loading: () => <Loading loading />,
 });
+const MobileSingleTool = dynamic(() => import('src/components/tool/mobile/MobileSingleTool'), {
+  loading: () => <Loading loading />,
+});
 
-export default SingleTool;
+const Single = () => {
+  return <>{!isMobile ? <MobileSingleTool /> : <SingleTool />}</>;
+};
+
+export default Single;
