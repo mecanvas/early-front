91f62116cddc4ffa2372a6899b1e8a57e958166a
김창회 <dudnqnfqlc@naver.com>
2021-07-24 19:08:58 +0900

    가이드 텍스트 오파시티 애니메이션 추가

--- src/components/tool/mobile/FirstStep.tsx
+++ src/components/tool/mobile/FirstStep.tsx
@@ -1,5 +1,6 @@
 import { Tabs, Divider } from 'antd';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState, useEffect } from 'react';
+import { useOpacity } from 'src/hooks/useOpacity';
 import { useAppSelector } from 'src/hooks/useRedux';
 import FirstFrameListByTab from './first/FirstFrameListByTab';
 import FirstSelectedList from './first/FirstSelectedList';
@@ -7,6 +8,8 @@ import { TabTitle, FirstGuideText } from './first/FirstStyle';
 
 const FirstStep = () => {
   const { frameInfoList } = useAppSelector(({ frame }) => frame);
+  const [isOpacityOn, setIsOpacityOn] = useState(false);
+  const { OpacityComponent } = useOpacity(isOpacityOn);
 
   const recommandList = useMemo(() => {
     return frameInfoList.filter((lst) => lst.recommand);
@@ -24,26 +27,39 @@ const FirstStep = () => {
 
   const handleTabClick = useCallback((key: string) => {
     setDefaultTab(key);
+    setIsOpacityOn(true);
   }, []);
+
+  useEffect(() => {
+    if (isOpacityOn) {
+      setIsOpacityOn(false);
+    }
+  }, [isOpacityOn]);
 
   return (
     <Tabs defaultActiveKey={defaultTab} onTabClick={handleTabClick} activeKey={defaultTab}>
       <Tabs.TabPane key="0" tab={<TabTitle>추천액자</TabTitle>}>
-        <FirstGuideText>이런 액자는 어떠세요?</FirstGuideText>
+        <OpacityComponent>
+          <FirstGuideText>이런 액자는 어떠세요?</FirstGuideText>
+        </OpacityComponent>
         <FirstFrameListByTab frameList={recommandList} />
         <Divider />
         <FirstSelectedList />
       </Tabs.TabPane>
 
       <Tabs.TabPane key="1" tab={<TabTitle>정사각형</TabTitle>}>
-        <FirstGuideText>정사각형 액자입니다. 탁상에 놓기 부담없는 사이즈에요!</FirstGuideText>
+        <OpacityComponent>
+          <FirstGuideText>정사각형 액자입니다. 탁상에 놓기 부담없는 사이즈에요!</FirstGuideText>
+        </OpacityComponent>
         <FirstFrameListByTab frameList={squareList} />
         <Divider />
         <FirstSelectedList />
       </Tabs.TabPane>
 
       <Tabs.TabPane key="2" tab={<TabTitle>직사각형</TabTitle>}>
-        <FirstGuideText>직사각형 액자입니다. 회전으로 가로와 세로를 바꿀 수 있어요!</FirstGuideText>
+        <OpacityComponent>
+          <FirstGuideText>직사각형 액자입니다. 회전으로 가로와 세로를 바꿀 수 있어요!</FirstGuideText>
+        </OpacityComponent>
         <FirstFrameListByTab frameList={rectangleList} />
         <Divider />
         <FirstSelectedList />
