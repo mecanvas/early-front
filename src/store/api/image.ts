7d1f3e0c331cb01c0f8fd30a8e79cfeb7846993c
김창회 <dudnqnfqlc@naver.com>
2021-07-25 23:10:46 +0900

    dataurl로 바꾸지않게 수정

--- src/store/api/image.ts
+++ src/store/api/image.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { ImgToDataURL } from 'src/utils/ImgToDataURL';
 import { putSelectedFrameImage } from '../reducers/frame';
 
 export const postImageUpload = createAsyncThunk<any, { type: 1 | 2; fd: any; id: number }>(
@@ -8,10 +7,9 @@ export const postImageUpload = createAsyncThunk<any, { type: 1 | 2; fd: any; id:
   async (data, { rejectWithValue, dispatch }) => {
     const { fd, type, id } = data;
     try {
-      const url = await axios.post<string>(`/canvas/single/upload`, fd).then((res) => {
+      const imgUrl = await axios.post<string>(`/canvas/single/upload`, fd).then((res) => {
         return res.data;
       });
-      const imgUrl = await ImgToDataURL(url);
       await dispatch(putSelectedFrameImage({ type, id, imgUrl }));
     } catch (err: any) {
       return rejectWithValue(err.response.data);
