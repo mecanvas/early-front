455cff8360701c12cb851330545cca80d00e7240
김창회 <dudnqnfqlc@naver.com>
2021-08-08 21:50:16 +0900

    이미지 1MB 이상

--- src/utils/imgSizeChecker.ts
+++ src/utils/imgSizeChecker.ts
@@ -5,6 +5,6 @@ export const imgSizeChecker = (file: File) => {
   if (file.size > IMG_LIMIT_MINIMUM_SIZE) {
     return true;
   }
-  alert('이미지는 최소 500kB이상이어야 합니다.');
+  alert('이미지는 최소 1MB이상이어야 합니다.');
   return false;
 };
