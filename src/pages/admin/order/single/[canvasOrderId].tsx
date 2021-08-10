ca31beea652397df1c8369308c4aaba60f099b43
김창회 <dudnqnfqlc@naver.com>
2021-08-10 23:47:24 +0900

    어드민 페이지 전부적용

--- src/pages/admin/order/single/[canvasOrderId].tsx
+++ src/pages/admin/order/single/[canvasOrderId].tsx
@@ -1,4 +1,37 @@
+import axios from 'axios';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import dynamic from 'next/dynamic';
 const AdminCanvasOrderDetail = dynamic(() => import('src/components/admin/order/AdminCanvasOrderDetail'));
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const cookie = ctx.req.headers.cookie || '';
+  let user = null;
+
+  if (cookie) {
+    user = await axios
+      .get('/auth', {
+        headers: {
+          cookie,
+        },
+      })
+      .then((res) => res.data);
+  }
+
+  if (user?.role === 1) {
+    return {
+      props: {
+        user,
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: '/admin/login',
+    },
+    props: {},
+  };
+};
 
 export default AdminCanvasOrderDetail;
