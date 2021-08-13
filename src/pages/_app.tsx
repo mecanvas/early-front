6e7c4621db01bc39c21e127669d79193de416fc3
김창회 <dudnqnfqlc@naver.com>
2021-08-13 18:45:17 +0900

    어드민 페이지에서 올바르게 noindex,nofollow 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ import { isIE } from 'react-device-detect';
 import { icons } from 'public/icons';
 import { store } from 'src/store/config';
 import { Provider } from 'react-redux';
-import { DefaultSeo } from 'next-seo';
+import { DefaultSeo, NextSeo } from 'next-seo';
 import Head from 'next/head';
 import { MAIN_DESC, MAIN_IMAGE_URL, MAIN_TITLE, SITE_NAME } from 'src/constants/SeoOnly';
 import { useRouter } from 'next/router';
@@ -67,7 +67,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {pathname.includes('admin') ? (
-        <DefaultSeo nofollow={true} noindex={true} title="얼리21 어드민" />
+        <>
+          <DefaultSeo title="얼리21 어드민" />
+          <NextSeo nofollow={true} noindex={true} />
+        </>
       ) : (
         <>
           <DefaultSeo
