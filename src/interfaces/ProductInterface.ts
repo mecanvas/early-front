85b26b66c569a8189bea09d749b84c881ab9030d
김창회 <dudnqnfqlc@naver.com>
2021-09-28 18:43:38 +0900

    product 인터페이스 생성

---
+++ src/interfaces/ProductInterface.ts
@@ -0,0 +1,46 @@
+import { User } from './User';
+
+export interface ProductList {
+  id: number;
+  title: string;
+  meta: string;
+  price: number;
+  thumb: string;
+  status: 1 | 2; // 1= 판매중 2= 품절
+}
+
+export interface ProductSeo {
+  seoTitle: string;
+  seoDesc: string;
+  seoImg: string;
+  seoKeywords: string;
+}
+
+export interface ProductOption {
+  type: 1 | 2; // 1 = 단독형 2 = 조합형
+  options: { id: number; optionName: string; value: string[] }[] | null;
+}
+
+export interface DeliveryOption {
+  deliveryPrice: number;
+  additionalPrice: number;
+  limit: number; // 조건충족시
+}
+
+export interface Product extends ProductList {
+  /**
+   *  id: number;
+   * title: string;
+   * meta: string;
+   * price: number;
+   * thumb: string;
+   * status : 1 | 2;
+   *
+   */
+  subThumb: string[]; // 추가 썸네일
+  uploader: User; // 업로더
+  description: string; // 상품설명
+  productOption: ProductOption; // 단독 or 조합형 옵션
+  deliveryOption: DeliveryOption; // 배송 요금
+  seo: ProductSeo;
+}
---
+++ src/interfaces/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: number;
+  email: string;
+  username: string;
+  phone: string;
+  address: string;
+  addressDetail: string;
+}
