a30bceb2fff9d6971655915b4c25075e5eece101
김창회 <dudnqnfqlc@naver.com>
2021-09-28 18:12:57 +0900

    메인 페이지 단장 start

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Home = () => {
+  return <Container></Container>;
+};
+
+export default Home;
