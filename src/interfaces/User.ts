9adb95aec1e389ea75f76a340ce89318bb1ea6d3
김창회 <dudnqnfqlc@naver.com>
2021-09-29 00:25:17 +0900

    user interface 재정리

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,8 +1,28 @@
+import { ProductList } from './ProductInterface';
+
+export interface Coupon {
+  id: number;
+  name: string;
+  type: 1 | 2; // 1 = 원단위 2 = 퍼센트
+  discount: number;
+  expired: Date;
+}
+
+export interface Order {
+  id: number;
+  orderNo: number;
+  deliveryStatus: 1 | 2 | 3 | 4;
+  product: ProductList;
+  createAt: string;
+}
+
+export interface Cart {
+  id: number;
+  product: ProductList[];
+}
+
 export interface User {
   id: number;
   email: string;
   username: string;
-  phone: string;
-  address: string;
-  addressDetail: string;
 }
