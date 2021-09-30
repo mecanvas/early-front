d0193c0d2febf76a5b7e777352cfa85b0e933d58
김창회 <dudnqnfqlc@naver.com>
2021-10-01 01:14:09 +0900

    회원가입 폼 목업

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,3 +1,15 @@
 import Register from 'src/components/register/Register';
+import Head from 'next/head';
 
-export default Register;
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+      </Head>
+      <Register />
+    </>
+  );
+};
+
+export default Index;
