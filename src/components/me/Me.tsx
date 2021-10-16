32b4d6748407a22c416158536c2ee0c90a4ccdf6
김창회 <dudnqnfqlc@naver.com>
2021-10-16 21:25:45 +0900

    마이페이지 넷바

--- src/components/me/Me.tsx
+++ src/components/me/Me.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Me = () => {
-  return <div></div>;
+  return (
+    <div>
+      <aside>
+        <ul>
+          <li>마이페이지</li>
+          <li>주문 내역</li>
+          <li>장바구니</li>
+          <li>배송</li>
+          <li>문의하기</li>
+        </ul>
+      </aside>
+    </div>
+  );
 };
 
 export default Me;
